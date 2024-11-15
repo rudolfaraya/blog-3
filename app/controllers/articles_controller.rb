022c29b8e9cea70f70b6fc166177185571fd09c8
Rodolfo Araya <rudolfaray2@gmail.com>
2024-11-14 23:12:40 -0300

    Agregada acción de mostrar un artículo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    # params = {id: 1}
+    @article = Article.find(params[:id])
+  end
 end
