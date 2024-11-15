9dfb857e161865e47787ff0066ac3d3bef7c1874
Rodolfo Araya <rudolfaray2@gmail.com>
2024-11-14 23:33:20 -0300

    Agregado formulario de creación de artículo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,24 @@ class ArticlesController < ApplicationController
     # params = {id: 1}
     @article = Article.find(params[:id])
   end
+
+  # muestra el formulario
+  def new
+    @article = Article.new
+  end
+
+  # toma los params ya filtrados y crea el artículo: si se crea correctamente, lo muestro, si falla, muestro los errores en el mismo new
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def articles_params
+      params.require(:article).permit(:title, :body)
+    end
 end
